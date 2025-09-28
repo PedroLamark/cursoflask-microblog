a8c5cda9f4f8303c6c8c879dc0e07d8fd504debb
Pedro Lamark <pedroluccalamark@outlook.com>
2025-09-28 14:49:35 -0300

    first commit

---
+++ app/routes.py
@@ -0,0 +1,15 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/index')
+
+def index():
+    nome = 'Lucca Lamark'
+    dados = {'Profissao': 'Desenvolvedor', 'Empresa': 'LamarksCorp'}
+
+    return render_template('index.html', nome=nome, dados=dados)
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
