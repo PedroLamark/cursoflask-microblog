b3729384dd1cb00c63e299e5a22045cb654159f2
Pedro Lamark <pedroluccalamark@outlook.com>
2025-09-28 15:50:03 -0300

    mais trechos de código

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,37 @@
 from app import app
-from flask import render_template
+from flask import render_template, redirect, request, flash
 
-@app.route('/')
-@app.route('/index')
 
-def index():
-    nome = 'Lucca Lamark'
-    dados = {'Profissao': 'Desenvolvedor', 'Empresa': 'LamarksCorp'}
+@app.route('/', defaults={'nome':'User', 'profissao': 'Dev', 'empresa': 'LamarksCorp'})
+@app.route('/index', defaults={'nome':'User', 'profissao': 'Dev', 'empresa': 'LamarksCorp'})
+@app.route('/index/<nome>/<profissao>/<empresa>')
 
-    return render_template('index.html', nome=nome, dados=dados)
+def index(nome, profissao, empresa):
+    dados = {'Profissao': profissao, 'Empresa': empresa}
+
+    return render_template(
+        'index.html', 
+        nome=nome, 
+        dados=dados
+    )
 
 @app.route('/contato')
 def contato():
     return render_template('contato.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+
+@app.route('/autenticar', methods=['POST'])
+def autenticar():
+    user = request.form.get('user')
+    passwd = request.form.get('pass')
+
+    if(user == 'admin' and passwd == '1234'):
+        return f'usuario {user} & senha {passwd}'
+    else:
+        flash('Dados inválidos')
+        return redirect('/login')
+
